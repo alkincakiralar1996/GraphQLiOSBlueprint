5a560d96d08a5bc55cae3afba262a97f068f3d21
Alkın Çakıralar <a.cakiralar@gmail.com>
2023-12-19 23:18:05 +0300

    Initial Commit

---
+++ GraphQLiOSBlueprint/GraphQLiOSBlueprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLiOSBlueprintApp.swift
+//  GraphQLiOSBlueprint
+//
+//  Created by Alkın Çakıralar on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLiOSBlueprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
